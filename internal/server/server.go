9dac5c20d69421dfe0837e54d87cc0ba32abf111
Marcus Adams <8154210+adamsma@users.noreply.github.com>
2025-04-27 21:35:28 -0400

    implement basic server with response

--- internal/server/server.go
+++ internal/server/server.go
@@ -9,38 +9,53 @@ import (
 
 type Server struct {
 	Port     int
-	listener *net.Listener
+	listener net.Listener
 	closed   atomic.Bool
 }
 
 func Serve(port int) (*Server, error) {
 
-	var l *net.Listener
-	go func(l *net.Listener) {
+	l, err := net.Listen("tcp", ":"+strconv.Itoa(port))
 
-		lstr, err := net.Listen("tcp", ":"+strconv.Itoa(port))
-		l = &lstr
-		if err != nil {
-			log.Fatalf("could not open listening port: %s", err.Error())
-		}
-
-	}(l)
+	if err != nil {
+		log.Fatalf("could not open listening port: %s", err.Error())
+	}
 
 	s := Server{Port: port, listener: l}
 	s.closed.Store(false)
+
+	go s.listen()
 
 	return &s, nil
 
 }
 
 func (s *Server) Close() error {
+
+	s.listener.Close()
+	s.closed.Store(true)
+
 	return nil
 }
 
 func (s *Server) listen() {
 
+	for !s.closed.Load() {
+
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.Fatalf("could not accept connection: %s", err.Error())
+		}
+
+		go s.handle(conn)
+
+	}
 }
 
 func (s *Server) handle(conn net.Conn) {
+
+	msg := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello World!"
+	conn.Write([]byte(msg))
+	conn.Close()
 
 }
